396e4afec15736a94bd692c54e790344b4e78af4
lumramirezch <34113039+lumramirezch@users.noreply.github.com>
2018-05-19 21:29:21 +0200

    Create Growth_curves.py

---
+++ Growth_curves.py
@@ -0,0 +1,7 @@
+"""
+ Download the fitderiv 
+ PS Swain, K Stevenson, A Leary, LF Montano-Gutierrez, IBN Clark, J Vogel, and T Pilizota. 
+ Inferring time derivatives including growth rates using Gaussian processes Nat Commun 7 (2016) 13766
+ http://swainlab.bio.ed.ac.uk/software/fitderiv/
+"""
+
